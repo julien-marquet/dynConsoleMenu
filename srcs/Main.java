c4058d8c805897deaff13a9e89e11981d592347d
julien-marquet <julienmarquet9@gmail.com>
2019-05-18 02:24:03 +0200

    Get action by index

--- srcs/Main.java
+++ srcs/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] argv) {
-		Menu menu = new OneTimeMenu();
+		Menu menu = new Menu();
         Scanner in = new Scanner(System.in);
         menu.interract(in);
         in.close();
--- srcs/com/menu/Menu.java
+++ srcs/com/menu/Menu.java
@@ -16,30 +16,56 @@ public class Menu {
 
 	private	Hashtable<String, Action> getDefaultActions() {
 		Hashtable<String, Action> actions = new Hashtable<String, Action>();
-		actions.put("exit", new Exit("exit"));
-		actions.put("repeat", new Repeat("repeat"));
+		actions.put("exit", new Exit());
+		actions.put("repeat", new Repeat());
 		return (actions);
 	}
 
 	public Menu() {
 		this.actions = getDefaultActions();
 	}
+
 	public Menu(Hashtable<String, Action> actions) {
 		this.actions = actions;
 	}
+
+	private Action getActionByIndex(int userIndex, Scanner in)
+	throws NoSuchElementException, IllegalStateException, NullPointerException {
+		int		i;
+		Action	action;
+	
+		userIndex = in.nextInt();
+		Enumeration<Action> eht = actions.elements();
+		i = 1;
+		while (eht.hasMoreElements()) {
+			action = eht.nextElement();
+			if (i == userIndex) {
+				return (action);
+			}
+			i++;
+		}
+		throw new NoSuchElementException();
+	}
 	public Action selectAction(Scanner in)
 	throws ActionInputException {
-		String userKey;
+		String	userKey = null;
+		int		userIndex = -1;
 
 		try {
-			userKey = in.nextLine();
+			return (getActionByIndex(userIndex, in));
+		} catch (NoSuchElementException notAnInt) {
+			try {
+				userKey = in.nextLine();
+			} catch (NoSuchElementException notAString) {
+				throw new ActionInputException("Empty input");
+			}
 			if (this.actions.containsKey(userKey)) {
 				return (this.actions.get(userKey));
 			}
-		} catch (NoSuchElementException | IllegalStateException | NullPointerException e) {
+		} catch (IllegalStateException | NullPointerException inputError) {
 			throw new ActionInputException("Input error");
 		}
-		throw new ActionInputException("No match");
+		throw new ActionInputException("No actions matched the input");
 	}
 
 	public void	interract(Scanner in) {
@@ -56,6 +82,7 @@ public class Menu {
                 }
             }
             try {
+				System.out.println("Executing " + action);
                 action.execute();
             } catch (ActionExecutionException e) {
                 System.err.println(e.getMessage());
@@ -67,10 +94,13 @@ public class Menu {
 	}
 
 	public String toString() {
-		Enumeration	e = actions.elements();
+		Enumeration<Action>	e = actions.elements();
 		StringWriter writer = new StringWriter();
+		int i = 1;
+		writer.append("MENU\n");
 		while (e.hasMoreElements()) {
-			writer.append(e.nextElement().toString() + "\n");
+			writer.append("  " + i + " : " + e.nextElement().toString() + "\n");
+			i++;
 		}
 		return (writer.toString());
 	}
